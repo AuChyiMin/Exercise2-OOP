fa9a2bfeee5a65bd1a28bb297b8a814e0a982c23
AuChyiMin <81155425+AuChyiMin@users.noreply.github.com>
2021-03-29 19:24:18 +0800

    Update Main.java

--- EXERCISE 2/src/Exercise/Main.java
+++ EXERCISE 2/src/Exercise/Main.java
@@ -5,10 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 
 		Cat c = new Cat();
-		/*System.out.println(c.Breed);
-		System.out.println(c.Colour);
-		System.out.println(c.Size);
-		System.out.println(c.Coat);*/
 		System.out.println("Breed is " + c.Breed);
 		System.out.println("Colour is " + c.Colour);
 		System.out.println("Size is " + c.Size);
